f761188aa3b1d9c3bb25d0feed2c8f1e4d941d68
Aleksei Vlasov <mike40016@gmail.com>
2026-09-23 15:42:24 +0400

    Initial commit

---
+++ src/feedbacks.js
@@ -0,0 +1,31 @@
+module.exports = async function (self) {
+	self.setFeedbackDefinitions({
+		ChannelState: {
+			name: 'Example Feedback',
+			type: 'boolean',
+			label: 'Channel State',
+			defaultStyle: {
+				bgcolor: 0xff0000,
+				color: 0x000000,
+			},
+			options: [
+				{
+					id: 'num',
+					type: 'number',
+					label: 'Test',
+					default: 5,
+					min: 0,
+					max: 10,
+				},
+			],
+			callback: (feedback) => {
+				console.log('Hello world!', feedback.options.num)
+				if (feedback.options.num > 5) {
+					return true
+				} else {
+					return false
+				}
+			},
+		},
+	})
+}
---
+++ src/variables.js
@@ -0,0 +1,7 @@
+module.exports = function (self) {
+	self.setVariableDefinitions({
+		variable1: { name: 'My first variable' },
+		variable2: { name: 'My second variable' },
+		variable3: { name: 'Another variable' },
+	})
+}
